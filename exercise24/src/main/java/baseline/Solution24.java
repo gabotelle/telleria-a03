4c2f8d7645b0216c11d3d079a460d74b4d902442
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-01 18:22:21 -0400

    e24

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,24 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Print "Enter two strings and I'll tell you if they are anagrams:"
+Prompt "Enter the first string: "
+    save input into <word1>
+Prompt "Enter the second string: "
+    save input into <word2>
+If word1(length) != word2(length)
+    Print "<word1> and <word2> are not anagrams."
+sort <word1>
+sort <word2>
+if every character is equal
+    Print "<word1> and <word2> are anagrams."
+else
+    "<word1> and <word2> are  not anagrams."
+ */
+public class Solution24 {
+
+}
