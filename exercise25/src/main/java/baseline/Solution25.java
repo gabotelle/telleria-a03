ed7ca770cff574c45a0396c89e325a8240d545be
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-01 19:52:48 -0400

    e25

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,35 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Prompt the user for password
+    save input into <password>
+check strength
+    if length < 8
+        if only contains numbers
+            return 1
+        if only contains letters
+            return 2
+    else contains letters and numbers
+        if contains special character
+            return 4
+        else
+            return 3
+    return 0
+if strength 1
+    Print "The password <password> is a very weak password."
+if strength 2
+    Print "The password <password> is a weak password."
+if strength 1
+    Print "The password <password> is a strong password."
+if strength 1
+    Print "The password <password> is a very strong password."
+else
+    Print "The password <password> is an invalid string."
+ */
+public class Solution25 {
+    
+}
