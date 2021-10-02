ab32618656d588577f6e904182916d216c502de9
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-02 17:54:22 -0400

    e29

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,19 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Ask until user inputs valid input
+    Prompt "What is the rate of return"
+    Save input into <rate>
+    if invalid input
+        Print "Sorry. That's not a valid input."
+Calculate years
+    <years> = 72 / <rate>
+Print "It will take <years> years to double your initial investment."
+ */
+public class Solution29 {
+    
+}
