3dbb773c877ccc085d133e1c776549b54f9fc494
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-02 18:56:31 -0400

    e31

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,18 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Prompt "Resting Pulse:"
+    save into <restingPulse>
+Prompt "Age:"
+    save input <age>
+Print table header
+for i [55-95] by 5
+    <targetRate> = (((220 − <age>) − <restingPulse>) × <'i' / 100>) + <restingPulse>
+    print "<i>%         | <targetRate> bpm
+ */
+public class Solution31 {
+}
