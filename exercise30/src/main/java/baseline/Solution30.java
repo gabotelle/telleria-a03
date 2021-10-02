24fcfbd28d4341f8ddffc2b78e6bb1eeab0a1ec8
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-02 18:42:00 -0400

    e30

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -4,11 +4,26 @@ package baseline;
  *  Copyright 2021 Gabriel Telleria
  */
 
+import java.util.Scanner;
+
 /*
 for i [1-12]
     for j [1-12]
         print <i*j>
  */
 public class Solution30 {
-    
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void twelveTable(){
+        for (int i = 1; i <=12; i++) {
+            for (int j = 1; j <= 12; j++) {
+                System.out.printf("%5d", i*j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        twelveTable();
+    }
 }
