673102f09b19def55361e608c2bc0c1a4a590f45
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-02 14:55:13 -0400

    e27

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,30 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Prompt "Enter the first name:"
+    save input into <firstName>
+Prompt "Enter the last name:"
+    save input into <lastName>
+Prompt "Enter the ZIP code:"
+    save input into <zipCode>
+Prompt "Enter the employee ID:"
+    save input into <employeeID>
+Validate first name
+    if first name < 2 characters long
+        not valid
+Validate last name
+    if last name < 2 characters long
+        not valid
+Validate zip code
+    if zip code is not a number
+        not valid
+Validate employee ID
+    if ID not in format (AA-1234)
+        not valid
+ */
+public class Solution27 {
+}
