170626e0cc27fc1d3f2ccb91a686e5adf7c8e871
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-02 18:23:56 -0400

    e29

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -4,16 +4,33 @@ package baseline;
  *  Copyright 2021 Gabriel Telleria
  */
 
-/*
-Ask until user inputs valid input
-    Prompt "What is the rate of return"
-    Save input into <rate>
-    if invalid input
-        Print "Sorry. That's not a valid input."
-Calculate years
-    <years> = 72 / <rate>
-Print "It will take <years> years to double your initial investment."
- */
+import java.util.Scanner;
+
 public class Solution29 {
-    
+    private static final Scanner input = new Scanner(System.in);
+    //try
+    //    Prompt "What is the rate of return"
+    //    Save input into <rate>
+    //    <years> = 72 / <rate>
+    //    Print "It will take <years> years to double your initial investment."
+    // if input is zero or is not number
+    //        Print "Sorry. That's not a valid input."
+    public static void calculateYears(){
+        boolean flag = true;
+        while(flag) {
+            try {
+                System.out.print("What is the rate of return: ");
+                int userInput = Integer.parseInt(input.nextLine());
+                System.out.printf("It will take %d years to double your initial investment.", (72 / userInput));
+                flag = false;
+            } catch (NumberFormatException | ArithmeticException e) {
+                System.out.println("Sorry. That's not a valid input.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        calculateYears();
+    }
+
 }
