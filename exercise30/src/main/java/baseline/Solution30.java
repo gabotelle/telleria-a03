36570e2bd529a493e13d50c1be23e6bcbc2d211b
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-02 18:31:58 -0400

    e30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,14 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+for i [1-12]
+    for j [1-12]
+        print <i*j>
+ */
+public class Solution30 {
+    
+}
