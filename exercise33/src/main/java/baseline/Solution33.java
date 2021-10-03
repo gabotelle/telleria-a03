05ba9a6903d053cb9e4a62bff1665b3377ebb34a
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 13:41:28 -0400

    e33

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,14 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Create list
+    "Yes", "No", "Maybe", "Ask again later"
+Print "What's your question?"
+Print item from list
+ */
+public class Solution33 {
+}
