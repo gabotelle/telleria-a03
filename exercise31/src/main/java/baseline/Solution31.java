528bff6fe3c5f056d466efcf1f7694223c77d269
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 00:34:34 -0400

    e31

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -4,15 +4,52 @@ package baseline;
  *  Copyright 2021 Gabriel Telleria
  */
 
-/*
-Prompt "Resting Pulse:"
-    save into <restingPulse>
-Prompt "Age:"
-    save input <age>
-Print table header
-for i [55-95] by 5
-    <targetRate> = (((220 − <age>) − <restingPulse>) × <'i' / 100>) + <restingPulse>
-    print "<i>%         | <targetRate> bpm
- */
+import java.util.Scanner;
+
 public class Solution31 {
+    private static final Scanner input = new Scanner(System.in);
+
+    //Prompt "Resting Pulse:"
+    //    save into <restingPulse>
+    public static int getRestingPulse(){
+        while(true){
+            try{
+                System.out.print("Resting Pulse: ");
+                String restingPulse = input.nextLine();
+                return Integer.parseInt(restingPulse);
+            }
+            catch(NumberFormatException e){
+                System.out.println("Invalid input.");
+            }
+        }
+    }
+    //Prompt "Age:"
+    //    save input <age>
+    public static int getAge() {
+        while (true) {
+            try {
+                System.out.print("age: ");
+                String age = input.nextLine();
+                return Integer.parseInt(age);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input.");
+            }
+        }
+    }
+    //Print table header
+    //for i [55-95] by 5
+    //    <targetRate> = (((220 − <age>) − <restingPulse>) × <'i' / 100>) + <restingPulse>
+    //    print "<i>%         | <targetRate> bpm
+    public static void createTable(int restingPulse, int age){
+        System.out.printf("%nIntensity    | Rate%n-------------|--------%n");
+        for (int i = 55; i <= 95; i += 5) {
+            int targetRate = (int)((220 - age - restingPulse) * (i / 100.0) + restingPulse);
+            System.out.printf("%d%%          | %d bpm%n", i, targetRate);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        createTable(getRestingPulse(), getAge());
+    }
 }
