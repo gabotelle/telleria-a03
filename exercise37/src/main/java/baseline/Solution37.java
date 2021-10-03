562087167861dcf0aea8f6b3e03b56f4cabfdf4a
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 16:07:04 -0400

    e37

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,24 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Prompt "What's the minimum length?"
+    save input into <length>
+Prompt "How many special characters?"
+    save input into <specialNumber>
+Prompt "How many numbers?"
+    save input into <numbersNumber>
+Create password
+    for random number[<length>-<length+20>]
+        random between character, special, and numbers.
+                    numbers until <numbersNumber>
+                    special until <specialNumber>
+                    characters >= <numbersNumber> + <specialNumbers>
+            add character/number/special into <password>
+    Print "Your password is <password>
+*/
+public class Solution37 {
+}
