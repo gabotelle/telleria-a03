8a6d4b5b5545f0e0170a494081aed026dcc8dc6f
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 17:06:21 -0400

    e38

---
+++ exercise37/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution37.java,1/e/1e27462d08ba21031c038fb2495ff7444b2af1cb
---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,15 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Prompt "Enter a list of numbers, separated by spaces:"
+    save input into list <numbers>
+Filter list
+    delete any odd number from <numbers>
+Print "The even numbers are <numbers>
+ */
+public class Solution38 {
+}
