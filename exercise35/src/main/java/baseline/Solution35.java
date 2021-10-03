cec9ef2834452638487bcceeb87ce7749d92c03e
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 14:47:29 -0400

    e35

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -4,10 +4,35 @@ package baseline;
  *  Copyright 2021 Gabriel Telleria
  */
 
-/*
-Prompt until blank "Enter a name"
-    add input into <contestants>
-Print "The winner is... <randomContestant>
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution35 {
+    private static final Scanner input = new Scanner(System.in);
+    private static final Random rand = new Random();
+
+    //Prompt until blank "Enter a name"
+    //    add input into <contestants>
+    public static List<String> getContestants(){
+        ArrayList<String> contestants = new ArrayList<>();
+        while(true){
+            System.out.print("Enter a name: ");
+            String contestant = input.nextLine();
+            if(contestant.length() == 0){
+                return contestants;
+            }
+            contestants.add(contestant);
+        }
+    }
+    //Print "The winner is... <randomContestant>
+    public static void pickWinner(List<String> contestants){
+        int random = rand.nextInt(contestants.size());
+        System.out.printf("The winner is... %s", contestants.get(random));
+    }
+
+    public static void main(String[] args) {
+        pickWinner(getContestants());
+    }
 }
