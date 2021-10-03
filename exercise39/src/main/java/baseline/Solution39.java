24d8d6c1466fdbc831077c92816807842f63544b
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 18:43:47 -0400

    e39

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,35 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Solution
+     Print "Name         | Position      | Separation Date
+               -------------|---------------|------------------"
+           print Employee for all employees at correct <positions>
+    Create hashmap <employees> of -Employee-
+    do
+        Find firstLastName
+            for all <employees.lastName>
+                if <first> < <employees.lastName>
+                    <first> = <employees.lastName>
+        print <employee> with <first>
+            print <firstName> <LastName> | <position> | <separationDate>"
+        remove <employee>
+    while is not empty
+
+
+
+Employee
+    firstName
+    lastName
+    position
+    separation date
+
+
+ */
+public class Solution39
+{
+}
