27aaf54f8cc647d055c6d1187d88afc3889a3037
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 14:03:11 -0400

    e34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,22 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Create list
+    John Smith, Jackie Jackson, Chris Jones, Amanda Cullen Jeremy Goodwin
+Print "There are 5 employees:
+        John Smith
+        Jackie Jackson
+        Chris Jones
+        Amanda Cullen
+        Jeremy Goodwin"
+Prompt "Enter an employee name to remove:
+    save input into <employeeName>
+Remove <employeeName> from list
+Print updated list
+ */
+public class Solution34 {
+}
