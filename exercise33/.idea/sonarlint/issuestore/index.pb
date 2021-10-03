e2571b63f5f913ecc0c50e34d66cf0dfda369bcb
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 13:53:42 -0400

    e33

---
+++ exercise33/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+;
+gradlew.bat,2/a/2a45a911a8f1836b0b6c5b758962572012d8f8c3
+V
+&src/main/java/baseline/Solution33.java,4/6/4649031bd8812c161e4e94f5ffd3b85ba379270d
--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -4,11 +4,27 @@ package baseline;
  *  Copyright 2021 Gabriel Telleria
  */
 
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution33 {
+    private static final Scanner input = new Scanner(System.in);
+    private static final Random rand = new Random();
+
 /*
 Create list
     "Yes", "No", "Maybe", "Ask again later"
 Print "What's your question?"
 Print item from list
  */
-public class Solution33 {
+    public static void magic8Ball(){
+        String[] answers = {"Yes", "No", "Maybe", "Ask again later"};
+        System.out.printf("What's your question?%n> ");
+        input.nextLine();
+        System.out.printf("%n%s", answers[rand.nextInt(answers.length)]);
+    }
+
+    public static void main(String[] args) {
+        magic8Ball();
+    }
 }
