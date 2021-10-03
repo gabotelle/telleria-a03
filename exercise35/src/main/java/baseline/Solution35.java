6b98df66c0bfc7f3c76e41a339f95185041c523b
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 14:33:59 -0400

    e35

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,13 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Prompt until blank "Enter a name"
+    add input into <contestants>
+Print "The winner is... <randomContestant>
+ */
+public class Solution35 {
+}
