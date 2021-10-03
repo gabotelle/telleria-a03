c15ec08709f14b02cf541f13312efba2eaf62304
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-03 15:05:04 -0400

    e36

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,37 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+/*
+Prompt user until 'done' "Enter a number:"
+    add input into <numbers>
+Print "Numbers: <numbers>"
+Find average
+    <average> = add all <numbers>
+    <average> = <average> / <averageSize>
+Find minimum
+    <minimum> = <numbers[0]>
+    for all <numbers>
+        if <numbers[i]> < <minimum>
+            <minimum> = <numbers[i]>
+    return <minimum>
+Find maximum
+    <maximum> = <numbers[0]>
+    for all <numbers>
+        if <numbers[i]> > <maximum>
+            <maximum> = <numbers[i]>
+    return <maximum>
+Find standard deviation
+    for all <numbers>
+        add (<numbers[i]> - <average>)^2 to <standardDeviation>
+    return square root of (<standardDeviation> / <numbersSize>)
+Print results
+    Print "The average is <average>"
+    Print "The minimum is <minimum>"
+    Print "The maximum is <maximum>"
+    Print "The standard deviation is <standardDeviation>"
+*/
+public class Solution36 {
+}
